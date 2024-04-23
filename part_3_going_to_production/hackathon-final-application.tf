86cb91a8f0da6f65bfca988940baa77dfb65a2d0
rushil daya <rushil.daya+demo@conveyordata.com>
2024-04-23 09:39:18 +0000

    final tweaks

--- part_3_going_to_production/hackathon-final-application.tf
+++ part_3_going_to_production/hackathon-final-application.tf
@@ -1,3 +1,8 @@
+locals {
+  function_name = "llm_hackathon_test_lambda-${random_string.random.result}"
+}
+
+
 data "aws_iam_policy_document" "llm_hackathon_assume_role" {
   statement {
     effect = "Allow"
@@ -85,7 +90,7 @@ resource "aws_lambda_function" "llm_hackathon_test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename      = "lambda_function_payload.zip"
-  function_name = "llm_hackathon_test_lambda-${random_string.random.result}"
+  function_name = local.function_name
   role          = aws_iam_role.llm_hackathon_iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
   source_code_hash = data.archive_file.lambda.output_base64sha256
@@ -100,6 +105,7 @@ resource "random_string" "random" {
 }
 
 resource "aws_lambda_function_url" "function_url" {
-  function_name      = "llm_hackathon_test_lambda-${random_string.random.result}"
+  function_name      = local.function_name
   authorization_type = "NONE"
+  depends_on = [aws_lambda_function.llm_hackathon_test_lambda]
 }
